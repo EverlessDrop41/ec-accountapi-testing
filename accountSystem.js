ee6a0741e616494e4d24817d03992badc3de8fe1
EverlessDrop41 <tomfperegrine@googlemail.com>
2014-11-02 15:49:52 +0000

    Account system fully fixed

--- accountSystem.js
+++ accountSystem.js
@@ -1,15 +1,9 @@
-//Google login test for firebase
 var fb = new Firebase("https://everlesslychatting.firebaseio.com/");
 var authData = fb.getAuth();
 
 var userName = "";
 var userEmail = "";
 var accountType = "";
-
-fb.on('child_added', function(snapshot) {
-    var message = snapshot.val();
-    displayChatMessage(message.name, message.text);
-});
 
 fb.onAuth(function(authInfo){
     authData = fb.getAuth();
@@ -33,44 +27,48 @@ $(document).ready(function(){
     fb.unauth();
   });
   accountCheck();
-})
+});
 
 function accountCheck(){
     if(authData){
-    //User is logged in
-    $("#loginSection").hide();
-    $("logout").show();
-    accountType = authData.provider;
-    console.log(accountType);
-    switch (accountType){
-      case "google":
-        userName = authData.google.displayName;
-        userEmail = authData.google.email;
-        displayAccountInfo();
-        break;
-      case "github":
-        userName = authData.github.username;
-        userEmail = authData.github.email;
-        displayAccountInfo();
-        break;
-      case "twitter":
-        userName = authData.twitter.displayName;
-        userEmail = "Twitter does not let us access email";
-        displayAccountInfo();
-        break;
-      case "facebook":
-        userName = authData.facebook.displayName;
-        userEmail = authData.facebook.email;
-        displayAccountInfo();
-        break;
-      default :
-        $("logout").hide();
+        //User is logged in
+        $("#accountInfo").show();
+        $("#loginSection").hide();
+        $("#messageInput").show();
+        $("#logout").show();
+        accountType = authData.provider;
+        console.log(accountType);
+        switch (accountType){
+          case "google":
+            userName = authData.google.displayName;
+            userEmail = authData.google.email;
+            displayAccountInfo();
+            break;
+          case "github":
+            userName = authData.github.username;
+            userEmail = authData.github.email;
+            displayAccountInfo();
+            break;
+          case "twitter":
+            userName = authData.twitter.displayName;
+            userEmail = "Twitter does not let us access email";
+            displayAccountInfo();
+            break;
+          case "facebook":
+            userName = authData.facebook.displayName;
+            userEmail = authData.facebook.email;
+            displayAccountInfo();
+            break;
+          default :
+            $("logout").hide();
+        }
     }
-  }
-  else{
-    $("#loginSection").show();
-    $("logout").hide();
-  }
+    else{
+        $("#loginSection").show();
+        $("#messageInput").hide();
+        $("#logout").hide();
+        $("#accountInfo").hide();
+    }
 }
 
 function login(accountSys){
@@ -118,12 +116,7 @@ function login(accountSys){
 }
 
 function displayAccountInfo(){
-  accountData = "Name: " + userName + "<br>" + "Email: " + userEmail + "<br>" + "Account Type: " + accountType;
+  accountData = "Hello " + userName + " you are logged in using " + accountType + ".";
   console.log(accountData);
-  $("#accountInfo").append(accountData);
+  $("#accountInfo").text(accountData);
 }
-
-function displayChatMessage(name, text) {
-  $messageToDisplay = "<div class='message'> <em class='userNameDisplayer'>" + name + "</em>  :" + "&#9;&#9;&#9;" + text + "</div>";
-  $('#messageBox').prepend($messageToDisplay);
-};
---
+++ messageHandler.js
@@ -0,0 +1,67 @@
+//Google login test for firebase
+var fb = new Firebase("https://everlesslychatting.firebaseio.com/");
+
+
+$(document).ready(function(){
+    //Listen for send request
+    $("#sendButton").click(function() {
+        submitMessageToFireBase();
+    });
+
+    $('#messageInput').keypress(function(e){
+        if (e.keyCode == 13 ){
+            submitMessageToFireBase()
+        }
+    });
+    
+    function submitMessageToFireBase(){
+        var name = userName;
+        var text = $("#messageInput").val().trim();
+        alert(text);
+        isNameValid = isValidMsg(name);
+        isMsgValid = isValidMsg(text);
+        if (isNameValid && isMsgValid) {
+          fb.push({
+            name: name,
+            text: text
+          });
+          $('#messageInput').val('');
+        }
+        else {
+            alert("Invalid inputs \n" + "Is name valid: " + boolToYN(isNameValid) + 
+            "\n" + "Is message valid: " + boolToYN(isMsgValid));
+        }
+    }
+});
+
+fb.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+});
+
+function displayChatMessage(name, text) {
+  $messageToDisplay = "<div class='message'> <em class='userNameDisplayer'>" + name + "</em>  :" + "&#9;&#9;&#9;" + text + "</div>";
+  $('#messageBox').prepend($messageToDisplay);
+};
+
+function isValidMsg(msg) {
+  var spaces = 0;
+  //Check if value is a string
+  if (!(typeof msg == "string")) {
+   //alert("String not detected");
+    return false;
+  }
+
+  if (msg.trim().length == 0) {
+    return false;
+  }
+  return true;
+}   
+
+function boolToYN(boolVal) {
+  if (boolVal == true) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
